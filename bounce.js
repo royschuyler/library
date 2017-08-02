9c77ff964bfa70ef5c5d985c8732259d2300c943
royschuyler <royschuyler@yahoo.com>
2017-08-02 13:04:43 -0700

    back and fourth bounce

---
+++ bounce.js
@@ -0,0 +1,21 @@
+var bounce = 4;
+//D NEEDS TO BE ODD
+function backAndFourth(d,start,bounce){
+  var arr = [];
+  var peak = d/2;
+  for(j=0;j<bounce;j++){
+    var k=-Math.floor(peak);
+    for(i=0;i<d;i++){
+      arr.push(k);
+      k++
+    }
+    var k=Math.floor(peak)-1
+    for(i=d-1;i>1;i--){
+      arr.push(k);
+      k--
+    }
+  }
+  return arr
+}
+
+console.log(backAndFourth(13,0,bounce))
